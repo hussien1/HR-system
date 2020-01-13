42e080e893440462402c5b9c000f402dabf8f267
Mahmoud Hanafy <43918955+mahmoudabdelhay@users.noreply.github.com>
2020-01-13 18:50:35 +0200

    keda 5elst el nav
    navbar has done 3la ra2y 7essen el ray2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+    
+    $('.closeAll').on('click', function() {
+        $(".hideOne,.hideTwo,.hideThree,.hideFour,.hideFive,.hideSix").hide();
+    });
+    
+    $('.tapOne').on('click', function() {
+        $(".hideOne").slideToggle();
+        $(".hideTwo,.hideThree,.hideFour,.hideFive,.hideSix").hide();
+    });
+
+    $('.tapTwo').on('click', function() {
+        $(".hideTwo").slideToggle();
+        $(".hideOne,.hideThree,.hideFour,.hideFive,.hideSix").hide();
+    });
+
+    $('.tapThree').on('click', function() {
+        $(".hideThree").slideToggle();
+        $(".hideOne,.hideTwo,.hideFour,.hideFive,.hideSix").hide();
+    });
+
+    $('.tapFour').on('click', function() {
+        $(".hideFour").slideToggle();
+        $(".hideOne,.hideTwo,.hideThree,.hideFive,.hideSix").hide();
+    });
+
+    $('.tapFive').on('click', function() {
+        $(".hideFive").slideToggle();
+        $(".hideOne,.hideTwo,.hideThree,.hideFour,.hideSix").hide();
+    });
+
+    $('.tapSix').on('click', function() {
+        $(".hideSix").slideToggle();
+        $(".hideOne,.hideTwo,.hideThree,.hideFour,.hideFive").hide();
+    });
+
+
+});
+
+/* ama ados c */
